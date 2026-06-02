c281fc6a048e7022bbf29baf65d58b6024447919
udithindrachapa <udithindrachapa26@gmail.com>
2026-06-02 16:27:29 +0530

    Fix errors in owner dashboard.

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { createBooking } from "../controllers/bookingController.js";
-import { getMyBookings } from "../controllers/booking.controller.js";
+import { getMyBookings, getOwnerBookings, updateBookingStatus } from "../controllers/booking.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, createBooking);
 router.get("/my", protect, getMyBookings);
+router.get("/owner", protect, getOwnerBookings);
+router.put("/:id", protect, updateBookingStatus);
 
 export default router;
